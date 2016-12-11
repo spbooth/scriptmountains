2e4592df982a3b4d7a45921ff56777c2cb73f9b2
Stephen Booth <s.booth@ed.ac.uk>
2016-12-11 14:41:53 +0000

    Add a stop/start buttton

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * 
  */
 window.onload = function() {
+	var running=true;
     // Get the canvas and context
     var canvas = document.getElementById("mountains"); 
     var context = canvas.getContext("2d");
@@ -9,30 +10,43 @@ window.onload = function() {
     // Define the image dimensions
     var width = canvas.width;
     var height = canvas.height;
+  
  
     // Create an ImageData object
     var imagedata = context.createImageData(width, height);
     var mount = new Mountain();
     var artist = new Artist(imagedata,width,height,mount);
     // Create the image
-    function createImage(offset) {
+    function createImage() {
         artist.plot_column();
     }
  
     // Main loop
-    function main(tframe) {
+    function main() {
        
  
         // Create the image
-        createImage(Math.floor(tframe / 10));
+        createImage();
  
         // Draw the image data to the canvas
         context.putImageData(imagedata, 0, 0);
         
-        // Request animation frames
-        window.requestAnimationFrame(main);
+        if( running ){
+         // Request animation frames
+         window.requestAnimationFrame(main);
+        }
     }
- 
+    var button = document.getElementById("toggle");
+    button.onclick = function(){
+    	running = ! running;
+    	if( running ){
+    		this.innerHTML="Stop";
+    		main();
+    	}else{
+    		this.innerHTML="Start";
+    	}
+    }
+    
     // Call the main loop
-    main(0);
+    main();
 };
