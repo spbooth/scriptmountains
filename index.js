2d36aca570b45eaf9974d3c3fd06c66adc401198
Stephen Booth <s.booth@ed.ac.uk>
2016-12-12 10:04:19 +0000

    start a control panel

--- index.js
+++ index.js
@@ -40,7 +40,24 @@ window.onload = function() {
          window.requestAnimationFrame(main);
         }
     }
-    var button = document.getElementById("toggle");
+    
+    var table = document.createElement("table");
+    controls.appendChild(table);
+    
+    function add_row(text,c){
+    	var row = document.createElement("tr");
+    	table.appendChild(row);
+    	var a = document.createElement("td");
+    	a.innerHTML=text;
+    	row.appendChild(a);
+    	var b = document.createElement("td");
+    	b.appendChild(c);
+    	row.appendChild(b);
+    };
+    
+    //var button = document.getElementById("toggle");
+    var button = document.createElement("button");
+    button.innerHTML="Stop"
     button.onclick = function(){
     	running = ! running;
     	if( running ){
@@ -50,7 +67,38 @@ window.onload = function() {
     		this.innerHTML="Start";
     	}
     }
+    add_row("Run control",button);
     
+    var fdim = document.createElement("input");
+    fdim.setAttribute("type","number");
+    fdim.setAttribute("min","0.5");
+    fdim.setAttribute("max","1.0");
+    fdim.setAttribute("step","0.01");
+    fdim.setAttribute("value",mount.fdim);
+    
+    fdim.onchange=function(){
+    	if( fdim.checkValidity()){
+    		mount.set_fdim(fdim.value);
+    	}
+    }
+    add_row("Fractal dimension",fdim);
+    
+    
+    var map = document.createElement("input")
+    map.setAttribute("type","checkbox");
+    map.checked=artist.draw_map;
+    map.onchange=function(){
+    	artist.draw_map=this.checked;
+    }
+    add_row("Draw map",map);
+    
+    var reflec = document.createElement("input")
+    reflec.setAttribute("type","checkbox");
+    reflec.checked=artist.reflec;
+    reflec.onchange=function(){
+    	artist.reflec=this.checked;
+    }
+    add_row("Draw reflections",reflec);
     // Call the main loop
     main();
 };
